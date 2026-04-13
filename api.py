a7c73c82bc6d972c0c4611b1fd10fac103ed5a0c
Rinkesh Amurte <rinkeshamrute23@gmail.com>
2026-04-13 23:36:06 +0530

    fix dynamic python port

--- api.py
+++ api.py
@@ -200,3 +200,9 @@ def view_user_reviews(session_id: str):
 @app.get("/health")
 def health():
     return {"status": "Reelogue is running"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
